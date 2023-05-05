3988e88b16510d6904051b12c33e9e300ee1381d
AnVlasova <anastasia.vlasova6293@gmail.com>
2023-05-05 16:06:46 +0300

    Add README for metadata generator file

--- tool/core/metadata_generator/run.py
+++ tool/core/metadata_generator/run.py
@@ -1,24 +1,19 @@
 import argparse
 import os
-import pandas as pd
 
 from tool.core.metadata_generator.generator import generate_metadata
 
 
-def train_classifiers():
+def run():
     parser = argparse.ArgumentParser(description='Generate metadata')
-    parser.add_argument('-d', '--embeddings', default='', required=True)
-    parser.add_argument('-probs', '--probabilities', default='', required=False)
+    parser.add_argument('-f', '--description_file', default='', required=False)
     args = parser.parse_args()
 
-    use_gt_for_training = not os.path.isfile(args.probabilities)
+    output_folder, _ = os.path.split(args.description_file)
 
-    path = '/home/vlasova/datasets/TrafficLightsDVC/description_pedestrian.json'
-    output_folder = '/home/vlasova/datasets/TrafficLightsDVC'
-
-    meta_file_path = generate_metadata(path, output_folder)
+    meta_file_path = generate_metadata(args.description_file, output_folder)
     print(meta_file_path)
 
 
 if __name__ == "__main__":
-    train_classifiers()
+    run()
